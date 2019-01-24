abf6e4914723ecbc05c2a2719a5df053871d4ad4
ShiMing <mingshi_sm@163.com>
2019-01-24 22:32:33 +0800

    add jwt token

--- tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
@@ -3,6 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
+import util.JwtUtil;
+
 @SpringBootApplication
 public class QaApplication {
 
@@ -14,5 +16,10 @@ public class QaApplication {
 	public IdWorker idWorkker(){
 		return new IdWorker(1, 1);
 	}
-	
+
+	@Bean
+    public JwtUtil jwtUtil() {
+	    return new JwtUtil();
+    }
+
 }
--- tensquare_qa/src/main/java/com/tensquare/qa/controller/ProblemController.java
+++ tensquare_qa/src/main/java/com/tensquare/qa/controller/ProblemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 /**
  * 控制器层
@@ -22,6 +23,8 @@ public class ProblemController {
 	@Autowired
 	private ProblemService problemService;
 
+	@Autowired
+	private HttpServletRequest request;
 
 	@RequestMapping(value = "/newlist/{labelid}/{page}/{size}", method = RequestMethod.GET)
 	public Result newList(@PathVariable String labelid, @PathVariable int page, @PathVariable int size ) {
@@ -90,6 +93,10 @@ public class ProblemController {
 	 */
 	@RequestMapping(method=RequestMethod.POST)
 	public Result add(@RequestBody Problem problem  ){
+	    String token = (String) request.getAttribute("claims_user");
+	    if(token == null || "".equals(token)) {
+	        return new Result(false, StatusCode.ACCESSERROR, "权限不足");
+        }
 		problemService.add(problem);
 		return new Result(true,StatusCode.OK,"增加成功");
 	}
